16a2010fd6a8a9f2108b622bea59b9be751c7e73
Raju Raj <89459126+Rajuraj07@users.noreply.github.com>
2021-09-27 18:17:32 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('#header').addClass('sticky');
+		} else {
+			$('#header').removeClass('sticky');
+		}
+	});
+
+	// Waypoints
+	$('.work').waypoint(function() {
+		$('.work').addClass('animated fadeIn');
+	}, {
+		offset: '75%'
+	});
+
+	// Fancybox
+	$('.work-box').fancybox();
+
+	// Flexslider
+	$('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
+
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
+
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
+
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
+	/*------------------
+		back to top
+	------------------*/
+		$(document).on('click', '.back-to-top', function() {
+		$("html,body").animate({
+		scrollTop: 0
+		}, 2000);
+	});
+	$(window).on('load', function() {
+	    /*-----------------
+	        back to top
+	    ------------------*/
+	    var backtoTop = $('.back-to-top')
+	    backtoTop.fadeOut();
+	});
+});
+
+//define variable for store last scrolltop
+    var lastScrollTop = '';
+
+    $(window).on('scroll', function() {
+
+        //back to top show/hide
+        var ScrollTop = $('.back-to-top');
+        if ($(window).scrollTop() > 1000) {
+            ScrollTop.fadeIn(1000);
+        } else {
+        ScrollTop.fadeOut(1000);
+    }
+});
